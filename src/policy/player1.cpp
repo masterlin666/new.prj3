48eef00b69ec1ded0b6238e86cb679159f33e974
masterlin666 <linmingyan31@gmail.com>
2023-06-18 15:03:54 +0800

    commit2

---
+++ src/policy/player1.cpp
@@ -0,0 +1,24 @@
+#include <cstdlib>
+
+#include "../state/state.hpp"
+#include "./player1.hpp"
+
+
+/**
+ * @brief Randomly get a legal action
+ * 
+ * @param state Now state
+ * @param depth You may need this for other policy
+ * @return Move 
+ */
+Move player1::get_move(State *state, int depth){
+  if(!state->legal_actions.size())
+    state->get_legal_actions();
+  
+  auto actions = state->legal_actions;
+  int k=depth;
+  for(int i=0;i<depth;i++){
+    k=rand()/k;
+  }
+  return actions[k%actions.size()];
+}
