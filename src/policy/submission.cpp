69f69f42b2d673e304e3c7a14a10eac134ff0197
masterlin666 <120867986+masterlin666@users.noreply.github.com>
2023-06-20 23:10:02 +0800

    Create submission.cpp

---
+++ src/policy/submission.cpp
@@ -0,0 +1,68 @@
+#include <cstdlib>
+#include <unordered_map>
+#include <iostream>
+using namespace std;
+#include "../state/state.hpp"
+#include "../commission.hpp"
+//typedef  pair < State , int >  prr;
+int value;
+std::unordered_map<std::string,int> transtable;
+
+int Alpha::alphabeta(State *state,int depth,int alpha,int beta,bool me){
+  state->get_legal_actions();
+  if(transtable.count(state->encode_state()))return transtable[state->encode_state()];
+  if(depth==0||state->legal_actions.empty()){
+    std::string key = state->encode_state();
+    transtable.insert({key,value});
+    return (me?state->evaluate():state->evaluate()*-1);
+  }
+  if(me){
+    
+    value=-1e6;
+    for(auto actions = state->legal_actions.begin();actions!=state->legal_actions.end();actions++){
+      value=std::max(value,alphabeta(state->next_state(*actions),depth-1,alpha,beta,!me));
+      alpha=std::max(value,alpha);
+      if(alpha>=beta)break;
+    }
+    std::string key = state->encode_state();
+    transtable.insert({key,value});
+    return value;
+  }
+  else {
+    value=1e6;
+    for(auto actions = state->legal_actions.begin();actions!=state->legal_actions.end();actions++){
+      value=std::min(value,alphabeta(state->next_state(*actions),depth-1,alpha,beta,!me));
+      beta=std::min(value,beta);
+      if(alpha>=beta)break;
+    }
+    std::string key = state->encode_state();
+    transtable.insert({key,value});
+    return value;
+  }
+
+}
+
+/**
+ * @brief Randomly get a legal action
+ * 
+ * @param state Now state
+ * @param depth You may need this for other policy
+ * @return Move 
+ */
+Move Alpha::get_move(State *state, int depth){
+  if(!state->legal_actions.size())
+    state->get_legal_actions();
+  
+
+  int lrg=-1e6;
+  Move bst;
+  for(auto actions = state->legal_actions.begin();actions!=state->legal_actions.end();actions++){
+    int result=alphabeta(state->next_state(*actions),depth-1,-1e6,1e6,0);
+
+    if(lrg<result){
+      lrg=result;
+      bst=*actions;
+    }
+  }
+  return bst;
+}
